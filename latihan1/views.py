cc8f926db0f7696c14e86035bfa7dcf8710bfbbf
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-03-13 19:58:32 +0800

    add template tag

--- latihan1/views.py
+++ latihan1/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 def index(request):
     context = {
         'judul':'kelas terbuka',
-        'kontributor':'Wahyu Permadi'
+        'kontributor':'Wahyu Permadi',
+        'nav': [
+            ['/','Admin'],
+            ['/about','About']
+        ]
     }
     return render(request, 'index.html', context)
